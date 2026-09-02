05fa5157d076bee7da8332c3ac960e499ff475e1
stevencampos <carlos.mendez.campos@est.una.ac.cr>
2026-09-02 17:29:06 -0600

    Table model

---
+++ src/main/java/proyecto/presentation/recursos/TableModel.java
@@ -0,0 +1,36 @@
+package proyecto.presentation.recursos;
+
+import proyecto.logic.Recurso;
+import proyecto.presentation.AbstractTableModel;
+
+import java.util.List;
+
+public class TableModel extends AbstractTableModel<Recurso> {
+
+    public static final int ID = 0;
+    public static final int CATEGORIA = 1;
+    public static final int DESCRIPCION = 2;
+
+    public TableModel(int[] cols, List<Recurso> rows) {
+        super(cols, rows);
+    }
+
+    @Override
+    protected void initColNames() {
+        colNames = new String[]{"Id", "Categoria", "Descripcion"};
+    }
+
+    @Override
+    protected Object getPropetyAt(Recurso element, int col) {
+        switch (cols[col]) {
+            case ID:
+                return element.getID();
+            case CATEGORIA:
+                return element.getCategoria();
+            case DESCRIPCION:
+                return element.getDescripcion();
+            default:
+                return null;
+        }
+    }
+}
