3c573cbed85c45c72f4534e7fd622b6cfc0d2bbd
ISAAC HERRERA ALFARO <isaac.herrera.alfaro@est.una.ac.cr>
2026-09-02 18:36:35 -0600

    Creacion del viewform de categorias SOLO .form

--- src/main/java/proyecto/presentation/categorias/View.java
+++ src/main/java/proyecto/presentation/categorias/View.java
@@ -1,4 +1,22 @@
 package proyecto.presentation.categorias;
 
+import javax.swing.*;
+
 public class View {
+    private JTextField BusqTxtFldDesc;
+    private JButton buscarButton;
+    private JButton imprimirButton;
+    private JTextField TxtFldID;
+    private JTextField CatTxtFldDesc;
+    private JButton guardarButton;
+    private JButton borrarButton;
+    private JButton limpiarButton;
+    private JTable table1;
+    private JPanel panel;
+    private JButton funcionariosButton;
+    private JButton categoriasButton;
+    private JButton recursosButton;
+    private JButton calendarizacionButton;
+    private JButton estadisticasButton;
+    private JButton actividadesButton1;
 }
