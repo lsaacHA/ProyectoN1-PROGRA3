539f95cf9b36ed30566b85d0297b8571182de9a4
ISAAC HERRERA ALFARO <isaac.herrera.alfaro@est.una.ac.cr>
2026-09-06 17:19:58 -0600

    parte local de mis archivos, borrar guardar, etc

--- src/main/java/proyecto/presentation/categorias/View.java
+++ src/main/java/proyecto/presentation/categorias/View.java
@@ -19,4 +19,17 @@ public class View {
     private JButton calendarizacionButton;
     private JButton estadisticasButton;
     private JButton actividadesButton1;
+
+    public JTextField getBusqTxtFldDesc() { return BusqTxtFldDesc; }
+    public JButton getBuscarButton() { return buscarButton; }
+    public JButton getImprimirButton() { return imprimirButton; }
+    public JTextField getTxtFldID() { return TxtFldID; }
+    public JTextField getCatTxtFldDesc() { return CatTxtFldDesc; }
+    public JButton getGuardarButton() { return guardarButton; }
+    public JButton getBorrarButton() { return borrarButton; }
+    public JButton getLimpiarButton() { return limpiarButton; }
+    public JTable getTable1() { return table1; }
+    public JPanel getPanel() { return panel; }
+
+
 }
