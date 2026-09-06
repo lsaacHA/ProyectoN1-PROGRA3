4458db1ae96a84e5f01857ebb685809470d80a99
Isaias Jimenez H <160548706+IsaiasJimenezH@users.noreply.github.com>
2026-09-06 17:00:17 -0600

    Revert "Datos xml"

--- src/main/java/proyecto/logic/Usuario.java
+++ src/main/java/proyecto/logic/Usuario.java
@@ -1,14 +1,6 @@
 package proyecto.logic;
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlID;
-import jakarta.xml.bind.annotation.XmlSeeAlso;
-
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlSeeAlso(Funcionario.class)
 public class Usuario {
-    @XmlID
     private String id;
     private String clave;
     private Rol rol;
