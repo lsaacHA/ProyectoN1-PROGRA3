cd69a5f82022ec1333689d6ae3f085d4a91d5a40
ISAAC HERRERA ALFARO <isaac.herrera.alfaro@est.una.ac.cr>
2026-09-08 20:53:53 -0600

    Calendarizacion view.form

---
+++ src/main/java/proyecto/presentation/calendarizacion/TableModel.java
@@ -0,0 +1,4 @@
+package proyecto.presentation.calendarizacion;
+
+public class TableModel {
+}
---
+++ src/main/java/proyecto/presentation/calendarizacion/View.java
@@ -0,0 +1,14 @@
+package proyecto.presentation.calendarizacion;
+
+import javax.swing.*;
+
+public class View {
+    private JPanel panel1;
+    private JPanel PanelMain;
+    private JTextField textField1;
+    private JButton button1;
+    private JComboBox comboBox1;
+    private JButton cargarButton;
+    private JButton imprimirButton;
+    private JTable table1;
+}
