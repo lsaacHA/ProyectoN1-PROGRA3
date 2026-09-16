c2db0d3cafe88446ee592e2f6e8f8ba71d574c1f
ISAAC HERRERA ALFARO <isaac.herrera.alfaro@est.una.ac.cr>
2026-09-16 10:18:54 -0600

    Quite una cosa que se veia doble en categorias

--- src/main/java/proyecto/presentation/categorias/View.java
+++ src/main/java/proyecto/presentation/categorias/View.java
@@ -18,12 +18,6 @@ public class View implements PropertyChangeListener {
     private JButton limpiarButton;
     private JTable table1;
     private JPanel panel;
-    private JButton funcionariosButton;
-    private JButton categoriasButton;
-    private JButton recursosButton;
-    private JButton calendarizacionButton;
-    private JButton estadisticasButton;
-    private JButton actividadesButton1;
 
     public View() {
         TxtFldID.setEditable(false); // el ID de la categoría siempre es autogenerado
